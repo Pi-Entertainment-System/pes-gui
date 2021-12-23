578403af8d6472378a4ab8a413aab8220edcc496
Neil Munday <neilmunday@users.noreply.github.com>
2021-12-23 17:20:14 +0000

    Formatting mod by Atom

--- python/pes/qml/pes.js
+++ python/pes/qml/pes.js
@@ -112,12 +112,12 @@ function goHome() {
 /**
  * Format bytes as human-readable text.
  * From: https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
- * 
+ *
  * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
  *           binary (IEC), aka powers of 1024.
  * @param dp Number of decimal places to display.
- * 
+ *
  * @return Formatted string.
  */
 function humanFileSize(bytes, si=false, dp=1) {
@@ -127,8 +127,8 @@ function humanFileSize(bytes, si=false, dp=1) {
     return bytes + ' B';
   }
 
-  const units = si 
-    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
     : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
   let u = -1;
   const r = 10**dp;
