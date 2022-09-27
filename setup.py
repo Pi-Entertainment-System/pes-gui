3abaf868b7cf71c707bffc73654a82bcf4998538
Neil Munday <neilmunday@users.noreply.github.com>
2022-09-27 23:46:18 +0100

    Updated required packages

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setuptools.setup(
     description='GUI package for PES written in PyQT',
     include_package_data=True,
     install_requires=[
+        'PyQt5',
         'setuptools'
     ],
     license='GPLv3',
